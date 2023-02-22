f5faab8d940baac5a61bf9e3832b6bca6aab3f98
alumjruizr <alum.jruizr.2021@iesalixar.org>
2023-02-22 16:31:30 +0100

    entregable + examen U7

---
+++ U7/Entregable_21_22_Ghibli/app.js
@@ -0,0 +1,104 @@
+//TODO: Botón carga vehículos que recupere los vehículos, muestre una tabla y los inserte en BBDD.
+//TODO: 
+
+let xhr;
+const READY_STATE_COMPLETE = 4;
+let mensajes;
+let vehiculosJson = [];
+
+window.onload = () => {
+	document
+		.getElementById("cargaVehiculos")
+		.addEventListener("click", cargaVehiculos);
+
+	document
+		.getElementById("registrarEnvio")
+		.addEventListener("click", cargaVehiculos);
+
+	mensajes = document.getElementById("mensajes");
+};
+
+function cargaVehiculos() {
+    console.log("Cargando vehículos")
+
+	if (XMLHttpRequest) {
+		xhr = new XMLHttpRequest();
+		xhr.onreadystatechange = comprobar;
+		xhr.open("GET", "vehiculos.json");
+		xhr.send();
+	}
+}
+
+function comprobar() {
+	console.log("comprobar");
+	if (xhr.readyState === READY_STATE_COMPLETE && xhr.status === 200) {
+		mensajes.innerHTML = "Datos desde XML cargados";
+		console.log("comprobar ok");
+		console.log(JSON.parse(xhr.responseText));
+		let vehiculos = JSON.parse(xhr.responseText);
+
+		// Lo hemos hecho de las dos maneras posibles. En el ejercicio no haría falta. Sólo una sería suficiente
+		// insertar_comunidades_fetch(comunidades_json);
+		insertarVehiculosXML(vehiculos);
+	}
+}
+
+function insertarVehiculosXML(vehiculos) {
+	console.log("insertarVehiculosXML");
+	// Lo hacemos con  XMLHttpRequest. No haría falta según el enunciado, solamente con uno de ellos.
+
+	xhr = new XMLHttpRequest();
+	xhr.open("POST", "insertar_vehiculos.php");
+	xhr.setRequestHeader("Content-type", "application/json");
+	let vehiculosJson = JSON.stringify(vehiculos);
+	xhr.onreadystatechange = () => {
+		if (xhr.readyState === READY_STATE_COMPLETE && xhr.status === 200) {
+			let vehiculoJson = JSON.parse(xhr.responseText);
+			console.log(vehiculoJson);
+			construirTabla(vehiculoJson);
+		}
+	};
+	xhr.send(vehiculosJson);
+}
+
+
+
+function construirTabla(vehiculosJson) {
+    let divTabla = document.getElementById("tabla");
+	divTabla.innerHTML = "";
+
+    let tabla = document.createElement("table");
+	tabla.setAttribute("style", "border-collapse: collapse; text-align: center");
+	let tr = document.createElement("tr");
+
+    let rotulos = [
+		"id",
+		"name",
+		"description",
+		"vehicle_class",
+	];
+
+    rotulos.forEach((rotulo) => {
+		let th = document.createElement("th");
+		th.setAttribute("style", "border: solid 2px");
+		th.appendChild(document.createTextNode(rotulo));
+		tr.appendChild(th);
+	});
+	tabla.appendChild(tr);
+
+    vehiculosJson.forEach((vehiculo) => {
+        let tr = document.createElement("tr");
+		for (const item in vehiculo) {
+			let td = document.createElement("td");
+			td.setAttribute("style", "border: solid 2px");
+			td.appendChild(document.createTextNode(vehiculo[item]));
+			tr.appendChild(td);
+		}
+
+		tabla.appendChild(tr);
+    })
+    
+    console.log("Hola")
+
+
+}
--- U7/Examen_21_22_Rick_Morty/js/app.js
+++ U7/Examen_21_22_Rick_Morty/js/app.js
@@ -1,52 +1,195 @@
+/* Variables del estado */
+
+let READY_STATE_UNINITIALIZED = 0;
+let READY_STATE_LOADING = 1;
+let READY_STATE_LOADED = 2;
+let READY_STATE_INTERACTIVE = 3;
+let READY_STATE_COMPLETE = 4;
+
+
+let HTTP_STATUS_OK = 200;
+let HTTP_STATUS_NOT_FOUND = 404;
+let HTTP_STATUS_SERVER_ERROR = 500;
+
+/* Petición XMLHttpRequest */
+
 let xhr;
-const READY_COMPLETE_STATE = 4;
 
-let peticiones = [];
+let personajes = [];
 
-window.onload = () => {
-    document
-            .getElementById("cargaXML")
-            .addEventListener("click", cargaXML);
-    document
-            .getElementById("cargaFecth")
-            .addEventListener("click", cargaFetch);
+window.onload = init;
+
+function init() {
+    console.log(personajes)
+    getID("xmlButton").addEventListener("click", cargaXML);
+    getID("fetchButton").addEventListener("click", cargaFetch);
+    getID("saveButton").addEventListener("click", cargaSave);
 }
 
-function cargar_datos_xml() {
-	console.log("cargaXML");
+function cargaSave() {
 
-	if (XMLHttpRequest) {
-		xhr = new XMLHttpRequest();
-		xhr.onreadystatechange = comprobar;
-		xhr.open("GET", "latest.json");
-		xhr.send();
-	}
+    let idHTML = parseInt(getID("select").value);
+
+    let character;
+
+
+    for (let i = 0; i < personajes.length; i++) {
+        if (personajes[i].id === idHTML) {
+            character = personajes[i]
+            console.log(character)
+        }
+
+    }
+
+    for (let i = 0; i < character.episode.length; i++) {
+        fetch(character.episode[i])
+            .then((response) => response.json())
+            .then((data) => {
+                getID("resultado").innerHTML += `<br>Episodio ${data.name} cargado`;
+                saveEpisode(data);
+            })
+            .catch((err) => console.log(err));
+    }
+
 }
 
-function comprobar() {
-	console.log("comprobar");
-	if (xhr.readyState === READY_STATE_COMPLETE && xhr.status === 200) {
-		resultados.innerHTML = "Datos desde XML cargados";
-		console.log("comprobar ok");
-		console.log(JSON.parse(xhr.responseText));
-		let characters = JSON.parse(xhr.responseText);
+function saveEpisode(episode) {
+    fetch("guardar_episodio_rm.php", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(episode),
+        })
+        .then((response) => {
+            if (response.ok) {
+                return response.json();
+            }
+        })
+        .then((data) => {
+            getID("resultado").innerHTML += "<br>" + data.resultado;
 
-		// Lo hemos hecho de las dos maneras posibles. En el ejercicio no haría falta. Sólo una sería suficiente
-		// insertar_comunidades_fetch(comunidades_json);
-		insertar_comunidades_xmlhttprq(filtrar_campos(characters));
-	}
+        })
+        .catch((err) => console.log(err));
 }
 
-function cargar_datos_fetch() {
-	console.log("cargaFecth");
-	fetch("latest.json")
-		.then((response) => {
-			if (response.ok) return response.json();
-		})
-		.then((characters) => {
-			resultados.innerHTML = "Datos desde fetch cargados";
-			console.log(characters);
-			insertar_comunidades_xmlhttprq(filtrar_campos(characters));
-		});
+function cargaFetch() {
+    personajes = [];
+    for (let i = parseInt(getID("min").value); i < parseInt(getID("max").value) + 1; i++) {
+        fetch(`https://rickandmortyapi.com/api/character/${i}`)
+            .then((response) => response.json())
+            .then((data) => {
+                personajes.push(data);
+                getID("resultado").innerHTML += `<br>Personaje ${data.name} cargado`;
+                processPersonajes(personajes);
+                generateSelect();
+            })
+            .catch((err) => console.log(err));
+
+    }
+
 }
 
+function cargaXML() {
+    personajes = [];
+    for (let i = parseInt(getID("min").value); i < parseInt(getID("max").value) + 1; i++) {
+        if (window.XMLHttpRequest) {
+            let xhr = new XMLHttpRequest();
+
+            xhr.onreadystatechange = checkStatus => {
+                if (xhr.readyState === READY_STATE_COMPLETE && xhr.status === HTTP_STATUS_OK) {
+                    let jsonContent = JSON.parse(xhr.responseText);
+                    personajes.push(jsonContent);
+                    getID("resultado").innerHTML += `<br>Personaje ${jsonContent.name} cargado`;
+                    processPersonajes(personajes);
+                    generateSelect();
+                };
+            }
+            xhr.open(
+                "GET",
+                `https://rickandmortyapi.com/api/character/${i}`
+            );
+            xhr.send();
+
+        } else {
+            alert("No tiene soporte para AJAX");
+        }
+    }
+
+}
+
+function processPersonajes(pjArray) {
+
+    let characters = getID("personajes");
+
+    characters.innerHTML = "";
+
+    for (let i = 0; i < pjArray.length; i++) {
+
+        let div = createElem("div");
+        characters.appendChild(div);
+
+        let img = createElem("img");
+        setAtt(img, "src", pjArray[i].image);
+        div.appendChild(img);
+
+        let name = createElem("h1");
+        let nameTxt = createTxt(pjArray[i].name);
+        name.appendChild(nameTxt);
+        div.appendChild(name);
+
+        let specie = createElem("p");
+        let specieTxt = createTxt(pjArray[i].species);
+        specie.appendChild(specieTxt);
+        div.appendChild(specie);
+
+        let location = createElem("p");
+        let locationTxt = createTxt(pjArray[i].location.name);
+        location.appendChild(locationTxt);
+        div.appendChild(location);
+
+        let created = createElem("p");
+        let createdTxt = createTxt(pjArray[i].created);
+        created.appendChild(createdTxt);
+        div.appendChild(created);
+    }
+
+}
+
+function generateSelect() {
+    let select = getID("select");
+
+    select.innerHTML = "";
+
+    for (let i = 0; i < personajes.length; i++) {
+
+        let option = createElem("option");
+        let optionTxt = createTxt(personajes[i].name);
+        setAtt(option, "value", personajes[i].id);
+        option.appendChild(optionTxt);
+        select.appendChild(option);
+    }
+}
+
+
+
+
+
+
+
+function createElem(e) {
+    return document.createElement(e);
+}
+
+function getID(id) {
+    return document.getElementById(id);
+}
+
+function setAtt(e, att, value) {
+    e.setAttribute(att, value);
+
+}
+
+function createTxt(txt) {
+    return document.createTextNode(txt);
+}
